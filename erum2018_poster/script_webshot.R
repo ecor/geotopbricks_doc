83862c807cc9204fc06e67fb9a1d8452751ae9e5
ecor <emanuele.cordano@gmail.com>
2018-05-21 17:06:21 +0200

    updates poster

--- erum2018_poster/script_webshot.R
+++ erum2018_poster/script_webshot.R
@@ -3,8 +3,8 @@
 #
 #
 
-file <- "erum2018_poster_cordano_et_al"
-file_rmd <- paste0(file,".Rmd")
+filez <<- "erum2018_poster_cordano_et_al"
+filez_rmd <- paste0(filez,".Rmd")
 
 ### A0: 
 
@@ -12,11 +12,11 @@ f <- 2.7
 width <- 841*f
 height <- 1189*f
 
-rmarkdown::render(file_rmd)
+postr::render(filez_rmd)
 
-file_png <- paste0(file,".png")
-file_url <- paste0(file,".html")
-file_pdf <- paste0(file,".jpg")
+#file_png <- paste0(filez,".png")
+#file_url <- paste0(filez,".html")
+#file_pdf <- paste0(filez,".jpg")
 
-webshot::webshot(url=file_url,file=file_png,vwidth = width,vheight = height)
+#webshot::webshot(url=file_url,file=file_png,vwidth = width,vheight = height)
 
