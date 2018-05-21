7fbedb4e7422dd9823c65f9b23d42bc9ba9dc11a
ecor <emanuele.cordano@gmail.com>
2018-05-21 16:51:26 +0200

    updates poster

--- erum2018_poster/cleaning_simulation.R
+++
@@ -1,3 +0,0 @@
-"resources/simulation/Matsch_B2_Ref_007" 
-"resources/simulation/Matsch_P2_Ref_007"
-"resources/simulation/Matsch_B2_Ref_007_GPPE" 
--- erum2018_poster/script_webshot.R
+++ erum2018_poster/script_webshot.R
@@ -3,11 +3,8 @@
 #
 #
 
-file <- "erum_poster_2"
+file <- "erum2018_poster_cordano_et_al"
 file_rmd <- paste0(file,".Rmd")
-file_png <- paste0(file,".png")
-file_url <- paste0(file,".html")
-file_pdf <- paste0(file,".jpg")
 
 ### A0: 
 
@@ -16,5 +13,10 @@ width <- 841*f
 height <- 1189*f
 
 rmarkdown::render(file_rmd)
-webshot::webshot(url=file_url,file=file_pdf,vwidth = width,vheight = height)
+
+file_png <- paste0(file,".png")
+file_url <- paste0(file,".html")
+file_pdf <- paste0(file,".jpg")
+
+webshot::webshot(url=file_url,file=file_png,vwidth = width,vheight = height)
 
